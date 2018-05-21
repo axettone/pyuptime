59278d53d117edaf7ac3c8fc4d3b99469b7ff7ec
Paolo Niccolò Giubelli <paoloniccolo.giubelli@gmail.com>
2018-05-21 22:18:33 +0200

    Enhancing add_site tool and documentation
    Signed-off-by: Paolo Niccolò Giubelli <paoloniccolo.giubelli@gmail.com>

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Implement email notifications.
 
 ## Setup
 Copy config.yml.example to config.yml and setup your SMTP parameters.
+You can add some sites using add_site.py script, doing something like
+	./add_site.py http://www.myurl.com alarms@mywebagency.com
 
 ## Requirements
 PyYAML is required.
--- add_site.py
+++ add_site.py
@@ -7,5 +7,8 @@ def put_website(conn, website):
 	conn.execute('''INSERT INTO websites (url,notifyemail) VALUES (?,?)''', (website.url, website.notifyemail))
 	conn.commit()
 
+if (len(sys.argv)<3):
+	print "Usage %s %s %s"%(sys.argv[0], "http://www.myexample.com", "alarm@mywebagency.com")
+	exit(-1)
 conn = sqlite3.connect('websites.db')
 put_website(conn,website.WebSite(sys.argv[1],sys.argv[2]))
